8878b3b845b47e4fd499afa77de026e1b83513f3
VoltixNepal <voltixnepal@gmail.com>
2026-09-23 20:32:50 +0545

    fix: add React useState and useEffect imports in AdminSidebar

--- components/admin/AdminSidebar.tsx
+++ components/admin/AdminSidebar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
 import {
